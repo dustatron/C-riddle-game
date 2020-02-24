85933e48bd3cd1b2e2ec13d10a745e5139c86a42
Student <example@example.com>
2020-02-24 09:28:18 -0800

    add a riddle to the app.

--- Riddle.cs
+++ Riddle.cs
@@ -10,7 +10,7 @@ class Program
     Console.WriteLine("Press 'Y' to start or return to 'Enter' to quit. ");
     string toStart = Console.ReadLine();
 
-    if(toStart == 'y' || toStart == 'Y')
+    if(toStart == "y" || toStart == "Y")
     {
       //start game
       Riddle();
@@ -21,8 +21,18 @@ class Program
     }
   }
 
-  static voic Riddle()
+  static void Riddle()
   {
-    Console.WriteLine("Riddle Test");
+    Console.WriteLine("What has no door, hinge, key, or lid, but a golden treasure inside in hid?");
+    string answer = Console.ReadLine();
+    if (answer == "egg" || answer == "an egg")
+    {
+      Console.WriteLine("Super job traveler!");
+    }
+    else
+    {
+      Console.WriteLine("NOPE! the answer was 'an Egg'");
+      Console.WriteLine("I don't want to say you suck, but if the shoe fits...");
+    }
   }
 }
